201619aa474b1b21ed7ca1bbd008db8ca893e0b3
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-10-18 15:59:56 +0900

    inquiry 作成

--- diary/urls.py
+++ diary/urls.py
@@ -5,5 +5,5 @@ app_name = 'diary'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('inquiry', Views.InquiryView.as_view(), name='inquiry'),
+    path('inquiry/', views.InquiryView.as_view(), name='inquiry'),
 ]
